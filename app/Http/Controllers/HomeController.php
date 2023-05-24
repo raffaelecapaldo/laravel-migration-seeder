d86e3098bd8deefc8ace90e8b0e944f5b6610748
raffaelecapaldo <raffaelecpl@gmail.com>
2023-05-24 15:25:32 +0200

    Added query for retrieve today trains

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Models\Train;
+
+
+
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('home');
+        $todayDate = Carbon::today()->format('Y-m-d');//Per fare il confronto nel db
+        Carbon::setLocale('it'); //Per mostrare la data in pagina
+        $data = [
+            'today' => Carbon::today()->isoFormat('D MMMM YYYY'),
+             'todayTrains' => Train::where('date_of_departure', 'Like', $todayDate.'%')->get()
+        ];
+        dd($data['todayTrains']);
+        return view('home', $data);
     }
 }
